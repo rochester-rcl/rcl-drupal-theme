1bcdcaaeaed34c15978fe610a0f9bdcd8690eb95
Sean Morris <smorris@library.rochester.edu>
2015-10-06 12:18:14 -0400

    File Paths & Viewport fixes
    Added the PHP path code to the static images on the theme. Added the
    viewport code to the php front page to enable mobile responsiveness.

--- page--front.tpl.php
+++ page--front.tpl.php
@@ -1,43 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
 
-    <title>Fixed Top Navbar Example for Bootstrap</title>
-
-
-    <!-- Custom styles for this template -->
-    <link href="css/master.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- UofR bar -->
     <nav class="uofr-nav">
         <div class="container">
-          <img class="nav-uofr-logo" src="uofr-logo.png">
+          <img class="nav-uofr-logo" src="<?php print $directory; ?>/uofr-logo.png" />
         </div>
     </nav>
 
     <!-- RCL navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top rcl-nav">
+    <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -48,7 +19,7 @@
           </button>
           <!-- Logo -->
           <a class="navbar-brand" href="#">
-            <img class="nav-rcl-logo" src="rcl-wordmark.png">
+          <img class="nav-rcl-logo" src="<?php print $directory; ?>/rcl-wordmark.png" />
           </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
@@ -72,7 +43,8 @@
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">My Accounts</a></li>
             <li><a href="#">Chat Online</a></li>
-            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Giving</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -96,8 +68,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?php print $directory; ?>/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+    <script src="<?php print $directory; ?>/assets/js/ie10-viewport-bug-workaround.js"></script>
