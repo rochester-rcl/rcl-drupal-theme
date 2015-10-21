cfd08dec1b510a4cad8de179a8c73af496fd3e59
Sean Morris <smorris@library.rochester.edu>
2015-10-21 10:12:10 -0400

    Article content type TPL added for news and stories

--- js/nav.js
+++ js/nav.js
@@ -2,11 +2,9 @@
 (function ($) {
 $(window).scroll(function() {
     if ($(this).scrollTop() > 100){
-        $('.uofrbar').addClass("up");
         $('.navbar-fixed-top').addClass("up");
     }
     else{
-        $('.uofrbar').removeClass("up");
         $('.navbar-fixed-top').removeClass("up");
     }
 });
