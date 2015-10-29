fb6629e7de11a99efb5bac9801c652af70929564
Sean Morris <smorris@library.rochester.edu>
2015-10-29 13:20:41 -0400

    Commit test 2

--- js/nav.js
+++ js/nav.js
@@ -2,16 +2,18 @@
 (function ($) {
 $(window).scroll(function() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     if ($(this).scrollTop() > 34){
 =======
     if ($(this).scrollTop() > 40){
 >>>>>>> parent of 81956bd... Changed JS for the header to make it default position
+=======
+    if ($(this).scrollTop() > 30){
+>>>>>>> parent of ba96fa7... More additions to template.
         $('.navbar').addClass("fixed");
-        $('.nav-spacer').addClass("show");
     }
     else{
         $('.navbar').removeClass("fixed");
-        $('.nav-spacer').removeClass("show");
     }
 });
 })(jQuery);
